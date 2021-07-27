95608296ef88f62e1af78bb1b1661b97af6cf466
qlead001 <qlead001@ucr.edu>
2021-07-26 17:42:32 -0700

    Add Bluetooth connection and separate files
    Add simple Bluetooth connection and data read/write capabilities for
    Bluetooth LE. This does not work for Bluetooth Classic devices such as
    the HC-05 so it will need to be replaced with the
    flutter_bluetooth_serial library.
    
    Separate the code from main.dart into ped.dart for pedometer specific
    code and bluetooth.dart for Bluetooth specific code.

---
+++ pedometer-app/lib/bluetooth.dart
@@ -0,0 +1,276 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+class BluetoothPage extends StatefulWidget {
+  BluetoothPage({Key? key}) : super(key: key);
+
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> devicesList = [];
+  final Map<Guid, List<int>> readValues = {};
+
+  @override
+  _BluetoothPageState createState() => _BluetoothPageState();
+}
+
+class _BluetoothPageState extends State<BluetoothPage> {
+  final _writeController = TextEditingController();
+  BluetoothDevice? _connectedDevice;
+  List<BluetoothService> _services = [];
+
+  _addDeviceTolist(final BluetoothDevice device) {
+    if (!widget.devicesList.contains(device)) {
+      print("Found device ${device.name}(${device.id})");
+      setState(() {
+        widget.devicesList.add(device);
+      });
+    }
+  }
+
+  Future _startScanning() async {
+    bool isScanning = await widget.flutterBlue.isScanning.first;
+    if (!isScanning) {
+      print("Starting scan...");
+      widget.flutterBlue.startScan();
+    } else {
+      print("Already scanning");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.flutterBlue.connectedDevices
+        .asStream()
+        .listen((List<BluetoothDevice> devices) {
+      for (BluetoothDevice device in devices) {
+        _addDeviceTolist(device);
+      }
+    });
+    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results) {
+        _addDeviceTolist(result.device);
+      }
+    });
+    //widget.flutterBlue.startScan();
+    _startScanning();
+  }
+
+  ListView _buildListViewOfDevices() {
+    List<Container> containers = [];
+    for (BluetoothDevice device in widget.devicesList) {
+      containers.add(
+        Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Text(device.name == '' ? '(unknown device)' : device.name),
+                    Text(device.id.toString()),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                child: const Text(
+                  'Connect',
+                ),
+                onPressed: () async {
+                  widget.flutterBlue.stopScan();
+                  await device.connect();
+                  List<BluetoothService> services =
+                      await device.discoverServices();
+                  setState(() {
+                    _services = services;
+                    _connectedDevice = device;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  ListView _buildConnectDeviceView() {
+    List<Container> containers = [];
+    for (BluetoothService service in _services) {
+      List<Widget> characteristicsWidget = [];
+      for (BluetoothCharacteristic characteristic in service.characteristics) {
+        characteristic.value.listen((value) {
+          print(value);
+        });
+        characteristicsWidget.add(
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(characteristic.uuid.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    ..._buildReadWriteNotifyButton(characteristic),
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: <Widget>[
+                        Text('Value: ' +
+                            widget.readValues[characteristic.uuid].toString()),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        );
+      }
+      containers.add(
+        Container(
+          child: ExpansionTile(
+              title: Text(service.uuid.toString()),
+              children: characteristicsWidget),
+        ),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  List<ButtonTheme> _buildReadWriteNotifyButton(
+      BluetoothCharacteristic characteristic) {
+    List<ButtonTheme> buttons = [];
+
+    if (characteristic.properties.read) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: ElevatedButton(
+              child: const Text('READ'),
+              onPressed: () async {
+                var sub = characteristic.value.listen((value) {
+                  setState(() {
+                    widget.readValues[characteristic.uuid] = value;
+                  });
+                });
+                await characteristic.read();
+                sub.cancel();
+              },
+            ),
+          ),
+        ),
+      );
+    }
+    if (characteristic.properties.write) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: ElevatedButton(
+              child: const Text('WRITE'),
+              onPressed: () async {
+                await showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text("Write"),
+                        content: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: TextField(
+                                controller: _writeController,
+                              ),
+                            ),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          ElevatedButton(
+                            child: const Text("Send"),
+                            onPressed: () {
+                              characteristic.write(
+                                  utf8.encode(_writeController.value.text));
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ElevatedButton(
+                            child: const Text("Cancel"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              },
+            ),
+          ),
+        ),
+      );
+    }
+    if (characteristic.properties.notify) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: ElevatedButton(
+              child: const Text('NOTIFY'),
+              onPressed: () async {
+                characteristic.value.listen((value) {
+                  widget.readValues[characteristic.uuid] = value;
+                });
+                await characteristic.setNotifyValue(true);
+              },
+            ),
+          ),
+        ),
+      );
+    }
+
+    return buttons;
+  }
+
+  ListView _buildView() {
+    if (_connectedDevice != null) {
+      return _buildConnectDeviceView();
+    }
+    return _buildListViewOfDevices();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text("Bluetooth"),
+        ),
+        body: _buildView(),
+      );
+}
---
+++ pedometer-app/lib/ped.dart
@@ -0,0 +1,169 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
+
+class PedometerPage extends StatefulWidget {
+  @override
+  _PedometerPageState createState() => _PedometerPageState();
+}
+
+class _PedometerPageState extends State<PedometerPage> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+  int _count = 0, _base = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+      _count = event.steps;
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+      if (_status == "walking") {
+        _base = _count;
+      }
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pedometer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.bluetooth),
+            tooltip: 'Bluetooth',
+            onPressed: () {
+              Navigator.pushNamed(context, "/bluetooth");
+            },
+          ),
+        ],
+      ),
+      body: PedometerDisplay(
+        steps: _steps,
+        currSteps: (_count - _base),
+        pedStatus: _status,
+      ),
+    );
+  }
+}
+
+class PedometerDisplay extends StatelessWidget {
+  const PedometerDisplay({
+    Key? key,
+    required this.steps,
+    required this.currSteps,
+    required this.pedStatus,
+  }) : super(key: key);
+
+  final String steps;
+  final String pedStatus;
+  final int currSteps;
+
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return Center(
+          child: Flex(
+            direction: orientation == Orientation.portrait
+                ? Axis.vertical
+                : Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Total Steps taken:',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    Text(
+                      steps,
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ]),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Steps taken:',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    Text(
+                      currSteps.toString(),
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ]),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Pedestrian status:',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    Icon(
+                      pedStatus == 'walking'
+                          ? Icons.directions_walk
+                          : pedStatus == 'stopped'
+                              ? Icons.accessibility_new
+                              : Icons.error,
+                      size: 100,
+                    ),
+                    Center(
+                      child: Text(
+                        pedStatus,
+                        style: pedStatus == 'walking' || pedStatus == 'stopped'
+                            ? TextStyle(fontSize: 25)
+                            : TextStyle(fontSize: 15, color: Colors.red),
+                      ),
+                    )
+                  ])
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
